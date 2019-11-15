9c21a74a87a2bc21beaac1164335771253af3b4c
Lei <leimengcheng@baidu.com>
2019-11-15 17:35:57 +0800

    add chrom extension template

---
+++ src/popup/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p>{{helloworld}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      helloworld: chrome.i18n.getMessage("helloWorld")
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+p {
+  font-size: 20px;
+}
+</style>
